6b27e60628136fbb28b81d65a14e93c4cc962e5d
Cassie <cassierasugu@gmail.com>
2023-06-05 20:24:23 +0300

    Header part

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import "./Navbar.css";
 import logoImg from "../../images/logo2.jpg";
+import {HiOutlineMenuAlt3} from "react-icons/hi";
 
 const Navbar = () => {
     const [toggleMenu, setToggleMenu] = useState(false);
