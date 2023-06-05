58350ea045b3a04dfc83941ab8d4c7fd792913fa
Cassie <cassierasugu@gmail.com>
2023-06-05 19:41:55 +0300

    navabar routes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-route-dom'
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Home from './pages/Home/Home';
+import Contact from "./pages/Contact/Contact";
+import Cocktails from "./pages/Cocktails/Cocktails"
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+<BrowserRouter>
+<Routes>
+        <Route path = "/" element = {<Home />}>
+          <Route path = "contact" element = {<Contact />} />
+          <Route path = "cocktails" element = {<Cocktails />} />
+        </Route>
+</Routes>
+</BrowserRouter>
   </React.StrictMode>
 );
 
