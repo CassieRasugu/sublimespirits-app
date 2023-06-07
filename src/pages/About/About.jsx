b66b6a358115f9884a7a587d1630cf7218e2cfc9
Cassie <cassierasugu@gmail.com>
2023-06-07 13:55:05 +0300

    about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./About.css";
-import aboutImg from "../../images/about-img.jpg";
+import aboutImg from "../../images/contact2.jpeg";
 
 const About = () => {
   return (
@@ -16,8 +16,8 @@ const About = () => {
           </div>
           <div className='about-text'>
             <h2 className='about-title fs-26 ls-1'>About SublimeSpirits</h2>
-            <p className='fs-17'> </p>
-            <p className='fs-17'> </p>
+            <p className='fs-17'>Immerse yourself in the captivating realm of cocktails at our website. Explore a diverse catalog of handcrafted drinks, carefully curated ingredients, and premium alcoholic beverages. Unleash your creativity, perfect your mixology skills, and elevate your drinking experience. Whether you're a seasoned bartender or an aspiring home mixologist, our platform is your gateway to extraordinary libations.</p>
+            <p className='fs-17'>Elevate your cocktail game and embark on a flavorful adventure with us. Cheers to memorable moments and remarkable drinks! </p>
           </div>
         </div>
       </div>
